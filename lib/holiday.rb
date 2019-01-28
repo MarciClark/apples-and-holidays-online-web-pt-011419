199b5f56b5bd0d1d8c04cc8a8bdc19b57f1539d1
Marci Clark <marcig612@gmail.com>
2019-01-28 06:53:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,6 +40,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash[sugartime]["donut_day"] = supply_array
   holiday_hash
 end
+
 add_new_holiday_with_supplies(holiday_supplies, :sugartime, :donut_day, ["Donuts", "sprinkles"])
 
 end
